7f9b8e800c7772699c197ee1e670bf5226f6d7e3
Akherv <51315773+Akherv@users.noreply.github.com>
2020-03-20 11:02:21 +0100

    maj js  encore

--- script.js
+++ script.js
@@ -3,8 +3,5 @@ let talk = document.querySelector('#talk');
 
 
 button.addEventListener('click', function(e){
-(talk.classList!=="scale")?
-    talk.classList.add("scale")
-    :
-    talk.classList.remove("scale")
+    talk.classList.toggle("scale")
 })
