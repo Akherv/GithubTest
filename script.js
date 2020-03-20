814f9fcfe5d9f6b599bb6655941e8896a054bc1e
Akherv <51315773+Akherv@users.noreply.github.com>
2020-03-20 14:39:52 +0100

    maj color

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let talk = document.querySelector('#talk');
 let water = document.querySelectorAll('.water');
 
 button.addEventListener('click', function(e){
-    button.classList.toggle("scale");
+    talk.classList.toggle("scale");
 })
 
 water.forEach(el => el.addEventListener('click', event => {
   console.log(event.target.classList[0]);
-  event.target.classList.toggle("scale");
+  event.target.classList.toggle("color");
 }));
 
